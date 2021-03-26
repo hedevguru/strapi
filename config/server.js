9c521d73686631e223dafcc13bbbf706388ffd10
hedevguru <mhe6802@gmail.com>
2021-03-26 13:37:30 -0400

    update server configuration

--- config/server.js
+++ config/server.js
@@ -2,8 +2,16 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    autoOpen: false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f967b8c8184d16a778b38a7bd84d20e8'),
     },
   },
+  "production": true,
+  "proxy": {
+    "enabled": false
+  },
+  "cron": {
+    "enabled": false
+  }
 });
