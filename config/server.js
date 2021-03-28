8d5cada6958f7b1047f758b8b63769e70285580b
hedevguru <mhe6802@gmail.com>
2021-03-28 13:21:50 -0400

    develop mode

--- config/server.js
+++ config/server.js
@@ -2,15 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    autoOpen: false,
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f967b8c8184d16a778b38a7bd84d20e8'),
     },
     path: '/dashboard'
-  },
-  "production": true,
-  "autoReload": {
-    "enabled": true
   },
   "proxy": {
     "enabled": false
